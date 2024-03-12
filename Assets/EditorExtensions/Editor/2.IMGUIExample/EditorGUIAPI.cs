b1a5c5a130957e61eecd89d9e85520fd5058e7b6
BlueSky <lsxk0830@163.com>
2024-03-12 21:16:40 +0800

    25.E0018. IMGUI 之 EditorGUI 常用组件（二）

--- Assets/EditorExtensions/Editor/2.IMGUIExample/EditorGUIAPI.cs
+++ Assets/EditorExtensions/Editor/2.IMGUIExample/EditorGUIAPI.cs
@@ -5,19 +5,52 @@ namespace EditorExtensions
 {
     public class EditorGUIAPI
     {
-        private Rect mLabelRect = new Rect(10, 50, 200, 20);
+        private Rect mLabelRect = new Rect(10, 70, 200, 20);
+        private Rect mTextFieldlRect = new Rect(10, 90, 200, 20);
+        private Rect mTextAreaRect = new Rect(10, 120, 200, 40);
+        private Rect mPasswordFieldRect = new Rect(10, 170, 200, 20);
+        private Rect mDropdownButtonRect = new Rect(10, 200, 200, 20);
+        private Rect mLinkButtonRect = new Rect(10, 230, 200, 20);
+        private Rect mToggleRect = new Rect(10, 260, 200, 20);
+        private Rect mToggleLeftRect = new Rect(10, 290, 200, 20);
+        private Rect mHelpBoxRect = new Rect(10, 320, 200, 20);
+        private Rect mColorFieldRect = new Rect(10, 350, 200, 20);
+        private Rect mBoundsFieldRect = new Rect(10, 380, 200, 20);
 
         private bool mDisableedGroupValue = false;
-        private float mJumpHeight = 0f;
+        private string mTextFieldValue;
+        private string mTextAreaValue;
+        private string mPasswordFieldValue;
+        private bool mToggleValue = false;
+        private Color mColorFieldValue;
+        private Bounds mBoundsFieldValue;
 
         public void Draw()
         {
-            EditorGUI.LabelField(mLabelRect, "LabelField");
-
             mDisableedGroupValue = EditorGUILayout.Toggle("Disableed Group", mDisableedGroupValue);
             EditorGUI.BeginDisabledGroup(mDisableedGroupValue == false);
             {
-                mJumpHeight = EditorGUILayout.FloatField("Jump Height", mJumpHeight);
+                EditorGUI.LabelField(mLabelRect, "LabelField");
+                mTextFieldValue = EditorGUI.TextField(mTextFieldlRect, mTextFieldValue);
+                mTextAreaValue = EditorGUI.TextArea(mTextAreaRect, mTextAreaValue);
+                mPasswordFieldValue = EditorGUI.PasswordField(mPasswordFieldRect, mPasswordFieldValue);
+
+                if (EditorGUI.DropdownButton(mDropdownButtonRect, new GUIContent("12345"), FocusType.Keyboard))
+                {
+                    Debug.Log("DropdownButton click");
+                }
+                if (EditorGUI.LinkButton(mLinkButtonRect, "LinkButton"))
+                {
+                    Debug.Log("LinkButton click");
+                }
+                mToggleValue = EditorGUI.Toggle(mToggleRect, mToggleValue);
+                mToggleValue = EditorGUI.ToggleLeft(mToggleLeftRect, "ToggleLeft", mToggleValue);
+
+                EditorGUI.HelpBox(mHelpBoxRect, "HelpBoxTest", MessageType.Error);
+
+                mColorFieldValue = EditorGUI.ColorField(mColorFieldRect, mColorFieldValue);
+
+                mBoundsFieldValue = EditorGUI.BoundsField(mBoundsFieldRect, mBoundsFieldValue);
             }
             EditorGUI.EndDisabledGroup();
         }
