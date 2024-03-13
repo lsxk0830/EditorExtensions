fbe5d572164e8e46526ac4bcfd09351c7b875763
BlueSky <lsxk0830@163.com>
2024-03-13 12:28:46 +0800

    27.E0019. IMGUI 之 EditorGUI 常用组件（三）

--- Assets/EditorExtensions/Editor/2.IMGUIExample/EditorGUIAPI.cs
+++ Assets/EditorExtensions/Editor/2.IMGUIExample/EditorGUIAPI.cs
@@ -16,6 +16,14 @@ namespace EditorExtensions
         private Rect mHelpBoxRect = new Rect(10, 320, 200, 20);
         private Rect mColorFieldRect = new Rect(10, 350, 200, 20);
         private Rect mBoundsFieldRect = new Rect(10, 380, 200, 20);
+        private Rect mBoundsIntFieldRect = new Rect(10, 420, 200, 20);
+        private Rect mCurveFieldRect = new Rect(10, 460, 200, 20);
+        private Rect mDelayedDoubleFieldRect = new Rect(10, 500, 200, 20);
+        private Rect mDoubleFieldRect = new Rect(10, 530, 200, 20);
+        private Rect mEnumFlagsFieldRect = new Rect(10, 560, 200, 20);
+        private Rect mEnumPopupRect = new Rect(10, 590, 200, 20);
+        private Rect mFoldoutRect = new Rect(210, 60, 300, 20);
+        private Rect mGradientFieldRect = new Rect(210, 90, 300, 20);
 
         private bool mDisableedGroupValue = false;
         private string mTextFieldValue;
@@ -24,35 +32,60 @@ namespace EditorExtensions
         private bool mToggleValue = false;
         private Color mColorFieldValue;
         private Bounds mBoundsFieldValue;
+        private BoundsInt mBoundsIntFieldValue;
+        private AnimationCurve mCurveFieldValue = new AnimationCurve();
+        private double mDoubleFieldValue;
+        private bool mFoldoutValue = true;
+        private Gradient mGradient = new Gradient();
+
+        private enum EnumFlagsFieldValue
+        {
+            A = 1, B, C, D, E, F,
+        }
+
+        private EnumFlagsFieldValue mEnumFlagsFieldValue;
 
         public void Draw()
         {
             mDisableedGroupValue = EditorGUILayout.Toggle("Disableed Group", mDisableedGroupValue);
-            EditorGUI.BeginDisabledGroup(mDisableedGroupValue == false);
+
+            mFoldoutValue = EditorGUI.Foldout(mFoldoutRect, mFoldoutValue, "折叠");
+            if (mFoldoutValue)
             {
-                EditorGUI.LabelField(mLabelRect, "LabelField");
-                mTextFieldValue = EditorGUI.TextField(mTextFieldlRect, mTextFieldValue);
-                mTextAreaValue = EditorGUI.TextArea(mTextAreaRect, mTextAreaValue);
-                mPasswordFieldValue = EditorGUI.PasswordField(mPasswordFieldRect, mPasswordFieldValue);
-
-                if (EditorGUI.DropdownButton(mDropdownButtonRect, new GUIContent("12345"), FocusType.Keyboard))
+                EditorGUI.BeginDisabledGroup(mDisableedGroupValue == false);
                 {
-                    Debug.Log("DropdownButton click");
+                    EditorGUI.LabelField(mLabelRect, "LabelField");
+                    mTextFieldValue = EditorGUI.TextField(mTextFieldlRect, mTextFieldValue);
+                    mTextAreaValue = EditorGUI.TextArea(mTextAreaRect, mTextAreaValue);
+                    mPasswordFieldValue = EditorGUI.PasswordField(mPasswordFieldRect, mPasswordFieldValue);
+
+                    if (EditorGUI.DropdownButton(mDropdownButtonRect, new GUIContent("12345"), FocusType.Keyboard))
+                    {
+                        Debug.Log("DropdownButton click");
+                    }
+                    if (EditorGUI.LinkButton(mLinkButtonRect, "LinkButton"))
+                    {
+                        Debug.Log("LinkButton click");
+                    }
+                    mToggleValue = EditorGUI.Toggle(mToggleRect, mToggleValue);
+                    mToggleValue = EditorGUI.ToggleLeft(mToggleLeftRect, "ToggleLeft", mToggleValue);
+
+                    EditorGUI.HelpBox(mHelpBoxRect, "HelpBoxTest", MessageType.Error);
+
+                    mColorFieldValue = EditorGUI.ColorField(mColorFieldRect, mColorFieldValue);
+
+                    mBoundsFieldValue = EditorGUI.BoundsField(mBoundsFieldRect, mBoundsFieldValue);
+                    mBoundsIntFieldValue = EditorGUI.BoundsIntField(mBoundsIntFieldRect, mBoundsIntFieldValue);
+                    mCurveFieldValue = EditorGUI.CurveField(mCurveFieldRect, mCurveFieldValue);
+                    //mDoubleFieldValue = EditorGUI.DelayedDoubleField(mDelayedDoubleFieldRect, mDoubleFieldValue);
+                    mDoubleFieldValue = EditorGUI.DoubleField(mDoubleFieldRect, mDoubleFieldValue);
+                    mEnumFlagsFieldValue = (EnumFlagsFieldValue)EditorGUI.EnumFlagsField(mEnumFlagsFieldRect, mEnumFlagsFieldValue);
+                    mEnumFlagsFieldValue = (EnumFlagsFieldValue)EditorGUI.EnumPopup(mEnumPopupRect, mEnumFlagsFieldValue);
+
+                    mGradient = EditorGUI.GradientField(mGradientFieldRect, mGradient);
                 }
-                if (EditorGUI.LinkButton(mLinkButtonRect, "LinkButton"))
-                {
-                    Debug.Log("LinkButton click");
-                }
-                mToggleValue = EditorGUI.Toggle(mToggleRect, mToggleValue);
-                mToggleValue = EditorGUI.ToggleLeft(mToggleLeftRect, "ToggleLeft", mToggleValue);
-
-                EditorGUI.HelpBox(mHelpBoxRect, "HelpBoxTest", MessageType.Error);
-
-                mColorFieldValue = EditorGUI.ColorField(mColorFieldRect, mColorFieldValue);
-
-                mBoundsFieldValue = EditorGUI.BoundsField(mBoundsFieldRect, mBoundsFieldValue);
+                EditorGUI.EndDisabledGroup();
             }
-            EditorGUI.EndDisabledGroup();
         }
     }
 }
