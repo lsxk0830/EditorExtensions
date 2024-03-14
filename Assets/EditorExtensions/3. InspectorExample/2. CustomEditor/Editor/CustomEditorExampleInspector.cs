71df6d7a23ce5e844a536fbc745be884a8df2878
BlueSky <lsxk0830@163.com>
2024-03-14 13:38:17 +0800

    Update CustomEditorExampleInspector.cs

--- Assets/EditorExtensions/3. InspectorExample/2. CustomEditor/Editor/CustomEditorExampleInspector.cs
+++ Assets/EditorExtensions/3. InspectorExample/2. CustomEditor/Editor/CustomEditorExampleInspector.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,25 +6,27 @@ namespace EditorExtensions
     [CustomEditor(typeof(CustomEditorExample))]
     public class CustomEditorExampleInspector : Editor
     {
-        CustomEditorExample mTarget
+        private CustomEditorExample mTarget
         {
-            get { return target as CustomEditorExample; }
+            get
+            {
+                return target as CustomEditorExample;
+            }
         }
-        
 
         public override void OnInspectorGUI()
         {
-            var hpRect = GUILayoutUtility.GetRect(18,18,"TextField");
-            EditorGUI.ProgressBar(hpRect, mTarget.HP,"HP");
-            
-            var rangeRect = GUILayoutUtility.GetRect(18,18,"TextField");
-            EditorGUI.ProgressBar(rangeRect, mTarget.Range,"Range");
+            var hpRect = GUILayoutUtility.GetRect(18, 18, "TextField");
+            EditorGUI.ProgressBar(hpRect, mTarget.HP, "HP");
+
+            var rangeRect = GUILayoutUtility.GetRect(18, 18, "TextField");
+            EditorGUI.ProgressBar(rangeRect, mTarget.Range, "Range");
 
             EditorGUILayout.BeginHorizontal("box");
-            EditorGUILayout.LabelField("角色名",GUILayout.Width(100));
+            EditorGUILayout.LabelField("角色名", GUILayout.Width(100));
             mTarget.RoleName = EditorGUILayout.TextArea(mTarget.RoleName);
             EditorGUILayout.EndHorizontal();
-            
+
             EditorGUILayout.ObjectField(serializedObject.FindProperty("OtherObj"));
             serializedObject.ApplyModifiedProperties();
         }
